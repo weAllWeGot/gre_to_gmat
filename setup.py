521fd48ae69582d2fd472f47bd8805e5215fa5db
tosin <tosin.sonuyi@gmail.com>
2018-05-02 22:36:55 -0400

    setup files for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gre2gmat',
+  packages = ['gre2gmat'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A library for calculating GMAT equivalent scores from GRE scores',
+  author = 'Olutosin Sonuyi',
+  author_email = 'tosin.sonuyi+gre2gmat@gmail.com',
+  url = 'https://github.com/weallwegot/gre_to_gmat', # use the URL to the github repo
+  download_url = 'https://github.com/weallwegot/gre_to_gmat/archive/0.1.0.tar.gz', # I'll explain this in a second
+  keywords = ['GMAT', 'GRE', 'Conversion', 'MBA', 'GMAC', 'ETS'], # arbitrary keywords
+  classifiers = [],
+)

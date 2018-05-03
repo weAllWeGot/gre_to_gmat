f2ded0c7e8b473edcbc29a01c9e39e04f69c2716
tosin <tosin.sonuyi@gmail.com>
2018-05-02 21:33:08 -0400

    add sanity checks for rough estimation functions

--- tests/test_gre_to_gmat.py
+++ tests/test_gre_to_gmat.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.main import validate_score_input,gre2gmat
+from src.main import validate_score_input,gre2gmat,rough_gre2gmat,rougher_gre2gmat
 from src.constants import MAX_GRE, MIN_GRE, BAD_INPUT_RETURN, MIN_GMAT, MAX_GMAT
 
 class TestScoreCalculation(unittest.TestCase):
@@ -8,13 +8,37 @@ class TestScoreCalculation(unittest.TestCase):
 	def setUp(self):
 		pass
 
-	def test_sanity_check(self):
+	def test_sanity_check_gre2gmat(self):
 		ranges = range(MIN_GRE,MAX_GRE,2)
 		for v in ranges:
 			for q in ranges:
 				gmat = gre2gmat(gre_verbal=v,gre_quant=q)
 
-				self.assertTrue(gmat >= MIN_GMAT and gmat <= MAX_GMAT,"GMAT Score: {} not between {} and {}".format(gmat,MIN_GMAT,MAX_GMAT))
+				self.assertTrue(gmat >= MIN_GMAT and gmat <= MAX_GMAT,
+					"GMAT Score: {} from verbal: {} and quant: {}\nnot between {} and {}".format(
+						gmat,v,q,MIN_GMAT,MAX_GMAT))
+
+
+	def test_sanity_check_rough_gre2gmat(self):
+		ranges = range(MIN_GRE,MAX_GRE)
+		for v in ranges:
+			for q in ranges:
+				gmat = rough_gre2gmat(gre_verbal=v,gre_quant=q)
+
+				self.assertTrue(gmat >= MIN_GMAT and gmat <= MAX_GMAT,
+					"GMAT Score: {} not between {} and {}".format(
+						gmat,MIN_GMAT,MAX_GMAT))
+
+
+	def test_sanity_check_rougher_gre2gmat(self):
+		ranges = range(MIN_GRE,MAX_GRE)
+		for v in ranges:
+			for q in ranges:
+				gmat = rougher_gre2gmat(gre_verbal=v,gre_quant=q)
+
+				self.assertTrue(gmat >= MIN_GMAT and gmat <= MAX_GMAT,
+					"GMAT Score: {} not between {} and {}".format(
+						gmat,MIN_GMAT,MAX_GMAT))
 
 
 if __name__ == '__main__':

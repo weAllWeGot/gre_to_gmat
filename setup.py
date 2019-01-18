37354fdd53f65474b6ca2205d3645d7c90300e6c
tosin <tosin.sonuyi@gmail.com>
2019-01-18 06:43:39 +0100

    update setup.py version number

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'gre2gmat',
-  packages = ['gre2gmat'], # this must be the same as the name above
-  version = '0.1.1',
-  description = 'A library for calculating GMAT equivalent scores from GRE scores',
-  author = 'Olutosin Sonuyi',
-  author_email = 'tosin.sonuyi+gre2gmat@gmail.com',
-  url = 'https://github.com/weallwegot/gre_to_gmat', # use the URL to the github repo
-  download_url = 'https://github.com/weallwegot/gre_to_gmat/archive/0.1.1.tar.gz', 
-  package_data ={'':['data_table.csv']} ,
-  include_package_data=True,
-  keywords = ['GMAT', 'GRE', 'Conversion', 'MBA', 'GMAC', 'ETS'], 
-  classifiers = [],
+    name='gre2gmat',
+    packages=['gre2gmat'],  # this must be the same as the name above
+    version='0.1.2',
+    description='A library for calculating GMAT equivalent scores from GRE scores',
+    author='Olutosin Sonuyi',
+    author_email='tosin.sonuyi+gre2gmat@gmail.com',
+    url='https://github.com/weallwegot/gre_to_gmat',  # use the URL to the github repo
+    download_url='https://github.com/weallwegot/gre_to_gmat/archive/0.1.1.tar.gz',
+    package_data={'': ['data_table.csv']},
+    include_package_data=True,
+    keywords=['GMAT', 'GRE', 'Conversion', 'MBA', 'GMAC', 'ETS'],
+    classifiers=[],
 )

7af5e301d255d43fd73f42f66099d4af1f2896ce
tosin <tosin.sonuyi@gmail.com>
2019-01-18 06:46:55 +0100

    update download url w/ version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Olutosin Sonuyi',
     author_email='tosin.sonuyi+gre2gmat@gmail.com',
     url='https://github.com/weallwegot/gre_to_gmat',  # use the URL to the github repo
-    download_url='https://github.com/weallwegot/gre_to_gmat/archive/0.1.1.tar.gz',
+    download_url='https://github.com/weallwegot/gre_to_gmat/archive/0.1.2.tar.gz',
     package_data={'': ['data_table.csv']},
     include_package_data=True,
     keywords=['GMAT', 'GRE', 'Conversion', 'MBA', 'GMAC', 'ETS'],
